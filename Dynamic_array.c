575895130373924ea45f5a69842e48b6eb4c7fd0
Prathyusha Mitte <70644694+prathyusha1401@users.noreply.github.com>
2021-02-21 23:38:46 +0530

    Update Dynamic_array.c

--- Dynamic_array.c
+++ Dynamic_array.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * This stores the total number of books in each shelf.
- */
+
 int* total_number_of_books;
 
-/*
- * This stores the total number of pages in each book of each shelf.
- * The rows represent the shelves and the columns represent the books.
- */
+
 int** total_number_of_pages;
 
 int main()
@@ -32,9 +27,7 @@ int main()
         scanf("%d", &type_of_query);
 
         if (type_of_query == 1) {
-            /*
-             * Process the query of first type here.
-             */
+          
             int shelf, pages;
             scanf("%d %d", &shelf, &pages);
             total_number_of_books[shelf]++;
